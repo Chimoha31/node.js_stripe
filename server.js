d99d453d02357ce9491c30046019ad8bd4783ee9
Chimoha31 <mokochii1108@gmail.com>
2022-03-27 16:20:24 -0700

    Done

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ const stripe = require("stripe")(
   "sk_test_51KhxNDDmqZkvExu3a7MVdNo9618V226SwbrkW0ioEr0Ql18pYu5PA4g9UIPCePSS5Od0D4b9Rlc2kCmKDOVSWFkA00BwpOeNt8"
 );
 
-const YOUR_DOMAIN = "localhost:3000";
+const YOUR_DOMAIN = "http://localhost:3000";
 
 app.use(express.static("public"));
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
-    const prices = await stripe.prices.list();
+    const prices = await stripe.prices.list({});
     // session=客が何を買ったのか、どういう状態にあるかというのを指す
     const session = await stripe.checkout.sessions.create({
-      lineitems: [{
+      line_items: [{
         price: prices.data[0].id,
         quantity:1,
       }],
-      mode: "subscription",
-      success_url: `{YOUR_DOMAIN}/succeaa.html?session_id={CHECKOUT_SESSION_ID}`,
+      mode: 'subscription',
+      success_url: `${YOUR_DOMAIN}/success.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${YOUR_DOMAIN}/cancel.html`,
     });
     // 成功したら303のredirectでsession.urlに飛ばす
     res.redirect(303, session.url);
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    console.log(err);
   }
 });
 
